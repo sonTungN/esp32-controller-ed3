f71d803bb2f4731e29a8f127b8213c8e0f5ba595
sontungN <s3979348@rmit.edu.vn>
2025-09-04 10:33:02 +0700

    init(first): initialize the esp32-controller

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);
+}
+
+void loop() {
+  Serial.println("Done setting up serial");
+  Serial.println("LED Blinking...");
+  digitalWrite(2, HIGH);
+  delay(1000);
+  digitalWrite(2, LOW);
+  delay(1000);
+}
